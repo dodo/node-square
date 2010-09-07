1afe32af20dffc8613f268420b1b359d802ada76
dodo <dodo.the.last@googlemail.com>
2010-09-07 20:27:51 +0200

    cleanup in holder folding animation

--- public/js/physics.js
+++ public/js/physics.js
@@ -4,6 +4,7 @@ $(function(){
 node2.initNode = function (_node) {
     var node = $(_node);
     var holder = node.find(".holder");
+    var buttons = holder.find(".buttons");
     var body = node.find(".body");
     var canvas = $("#canvas");
     var left, top;
@@ -19,54 +20,75 @@ node2.initNode = function (_node) {
         left = canvas.offset().left + canvas.width() / 2;
         top = canvas.offset().top + canvas.height() / 2;
     }
-
-    /*
-    left = canvas.offset().left + canvas.width() / 2;
-    top = canvas.offset().top + canvas.height() / 2;
-    */
-
-    node.css({left: left, top: top});
-    //node.css({left:canvas.width() / 2, top:canvas.height() / 2});
-    node.hover(
-        function () {$(this).addClass("fixed");},
-        function () {$(this).removeClass("fixed");}
+    var holder_out = false;
+    var h = Math.min(body.outerHeight(), buttons.outerHeight())+1;
+    // node setup
+    node.css({
+        left: left,
+        top: top,
+        height: h,
+            position:"absolute",
+        width:buttons.outerWidth()*2+body.outerWidth(),
+    }).hover(
+        function () /*handleIn*/  {$(this).addClass("fixed");},
+        function () { // handleOut
+            $(this).removeClass("fixed");
+            if(!holder_out) return;
+            holder_out = false;
+            var me = $(this).find(".holder");
+            if(me.parent().hasClass("moving")) return;
+            var mybuttons = me.find(".buttons");
+            var h = Math.min(mybuttons.outerHeight(),
+                             me.parent().find(".body").outerHeight())+1;
+            me.css("overflow", "hidden");
+            me.animate({
+                    width: node2.holder_width * 0.2,
+                    height: h,
+                }, node2.fold_time, function () {
+                    mybuttons.css("visibility","hidden");
+            }).parent().animate({
+                left:  '+=' + node2.holder_width,
+                width: '-=' + node2.holder_width,
+                height: h,
+            }, node2.fold_time);
+        }
     );
-    node2.holder_min_height = Math.max(node2.holder_min_height, parseInt(holder.height()));
-    var h = 13+parseInt(holder.parent().find(".body").height());
-    holder.width('20px');
-    holder.animate({width:'-=15px', height:h}, 100, function () {
-        $(this).find(".button").css("visibility","hidden");
-    }).parent().animate({height:h},100);
-    holder.hover(
-        function () {
-            if($(this).parent().hasClass("moving")) return;
-            var h = holder_min_height;
-            if($(this).parent().hasClass("root")) {
-                h -= $(this).find(".button").height()+7;
-            }
-            h = Math.max(h,13+parseInt($(this).parent().find(".body").height()));
-            $(this).find(".button").css("visibility","visible");
-            $(this).animate({width:'+=15px', height:h}, 50, function () {
-                $(this).css("overflow","visible");
-            }).parent().animate({left:'-=15px',width:'+=16px',height:h},50);},//handleIn
-        function () {
-            if($(this).parent().hasClass("moving")) return;
-            var h = 13+parseInt($(this).parent().find(".body").height());
-            $(this).css("overflow","hidden");
-            $(this).animate({width:'-=15px', height:h}, 50, function () {
-                $(this).find(".button").css("visibility","hidden");
-            }).parent().animate({left:'+=15px',width:'-=16px',height:h},50);} //handleOut
+    // buttons setup
+    buttons.css("visibility","hidden");
+    // holder setup
+    holder.css({
+        height: h,
+        overflow: "hidden",
+        width: node2.holder_width * 0.2,
+    }).hover(
+        function () { // handleIn
+            if(holder_out) return;
+            holder_out = true;
+            var me = $(this);
+            if(me.parent().hasClass("moving")) return;
+            var mybuttons = me.find(".buttons");
+            var h = Math.max(mybuttons.outerHeight(),
+                             me.parent().find(".body").outerHeight())+7;
+            mybuttons.css("visibility","visible");
+            me.animate({
+                    width: node2.holder_width,
+                    height: h,
+                }, node2.fold_time, function () {
+                    me.css("overflow", "visible");
+            }).parent().animate({
+                left:  '-=' + node2.holder_width,
+                width: '+=' + node2.holder_width,
+                height: h,
+            }, node2.fold_time);
+        }
     );
-    node.css({height:Math.max(body.height(), node2.holder_min_height)+1,
-               width:holder.width()+body.width()+23,
-               position:"absolute",
-             });
 };
 
 /// init
 node2.init_physics = function(){
-// TODO remove global vars or capitalize
-    node2.holder_min_height = 0;
+// TODO remove global vars or capitalize or  move to sub hash const
+    node2.fold_time = 50;
+    node2.holder_width = $("#protonode .holder").outerWidth();
     node2.springs_physics = springsPhysics.generate();
     node2.springs_physics.static();
 
